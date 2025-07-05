01b6282aaa03633740cb6cc9c08d24990889fa2e
Afaf Afridi <ibrahim.hasan00710@gmail.com>
2025-07-05 23:25:17 +0500

    credentials commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let password=prompt("Enter your password:");
+if(password==="TheRizzler"){
+    console.log("Access granted");
+    alert("Access granted");
+}else if(password===""){
+    console.log("Access denied");
+    alert("Access denied");
+}else{
+    console.log("Access denied");
+    alert("Access denied");
+}
